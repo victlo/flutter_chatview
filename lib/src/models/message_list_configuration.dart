34f9969e7f32501879674b415133c672e90eb93f
szetochan2002 <szetochan2002@gmail.com>
2023-07-25 03:24:12 +0800

    Changed Day Text

--- lib/src/models/message_list_configuration.dart
+++ lib/src/models/message_list_configuration.dart
@@ -95,7 +95,7 @@ class DefaultGroupSeparatorConfiguration {
   /// Used for giving text style of chat separator widget.
   final TextStyle? textStyle;
 
-  final String Function()? getDateText;
+  final String Function(DateTime dateTime)? getDateText;
 
   const DefaultGroupSeparatorConfiguration({
     this.padding,
